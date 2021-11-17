a673d66b05841e68b1da8a4964731eae84e56306
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-11-17 16:25:25 +0100

    Deploy

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -11,4 +11,4 @@ source ${RELEASE_PATH}/env
 
 _HELM_OPTS="-f values.yaml -f ${RELEASE_PATH}/config.yaml --namespace ${K8S_NAMESPACE} ."
 
-helm upgrade --create-namespace --reuse-values  --cleanup-on-fail -i ${HELM_RELEASE} ${_HELM_OPTS} 
+helm upgrade --create-namespace --reuse-values --cleanup-on-fail -i ${HELM_RELEASE} ${_HELM_OPTS} 
