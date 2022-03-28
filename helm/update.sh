658ac34569cde82aa1563678d51c4fbab7d4bb23
slapshin <staslapshin88@gmail.com>
2022-03-28 21:55:30 +0300

    refactor: update script

--- helm/update.sh
+++ helm/update.sh
@@ -9,6 +9,6 @@ RELEASE_PATH="./releases/${1}"
 
 source ${RELEASE_PATH}/env
 
-_HELM_OPTS="${@:2} -f ${RELEASE_PATH}/config.yaml --namespace ${K8S_NAMESPACE} ."
+_HELM_OPTS="${@:2} --namespace ${K8S_NAMESPACE} ."
 
-helm upgrade --reuse-values --cleanup-on-fail ${HELM_RELEASE} ${_HELM_OPTS}; 
+helm upgrade --reuse-values ${HELM_RELEASE} ${_HELM_OPTS}; 
